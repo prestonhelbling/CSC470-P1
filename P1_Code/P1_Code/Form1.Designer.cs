55ba71dd30c9fabe5dae0dada62f1482e0317a7e
anderzonz23 <anderzonz23@hotmail.com>
2020-08-25 19:06:22 -1000

    Changed form name, form text, added Hello World label

--- P1_Code/P1_Code/Form1.Designer.cs
+++ P1_Code/P1_Code/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace P1_Code
 {
-    partial class Form1
+    partial class FormHelloWorld
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 85F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(18, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(646, 128);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hello World";
+            // 
+            // FormHelloWorld
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(680, 338);
+            this.Controls.Add(this.label1);
+            this.Name = "FormHelloWorld";
+            this.Text = "CSC 470 - P1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- P1_Code/P1_Code/Form1.cs
+++ P1_Code/P1_Code/Form1.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace P1_Code
 {
-    public partial class Form1 : Form
+    public partial class FormHelloWorld : Form
     {
-        public Form1()
+        public FormHelloWorld()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CenterToScreen();
         }
     }
 }
