f6e296c5e860ffc45419462799cebf05bd636aad
anderzonz23 <anderzonz23@hotmail.com>
2020-08-25 18:27:25 -1000

    Updated form name, updated form text, added Hello World label

---
+++ P1 Code/P1 Code/FormHelloWorld.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace P1_Code
+{
+    partial class FormHelloWorld
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 85F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(646, 128);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hello World";
+            // 
+            // FormHelloWorld
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(716, 345);
+            this.Controls.Add(this.label1);
+            this.Name = "FormHelloWorld";
+            this.Text = "CSC 470 - P1 ";
+            this.Load += new System.EventHandler(this.FormHelloWorld_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
